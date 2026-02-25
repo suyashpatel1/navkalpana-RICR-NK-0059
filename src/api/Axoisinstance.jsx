9d9b5b042eb410b80bcc3514de4aa997924a4ae6
uditsharmahere <uditsharma7617@gmail.com>
2026-02-25 14:41:25 +0530

    Attendacne Module frontend design completed

--- src/api/Axoisinstance.jsx
+++ src/api/Axoisinstance.jsx
@@ -4,6 +4,8 @@ const API = axios.create({
   baseURL: "http://localhost:8080/api"
 });
 
+
+// hello 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
